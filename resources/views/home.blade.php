8e338ffd2578a6fb71a262ae01b174cdcd91a62a
ricardo <ricardo@hotmail.com>
2018-08-15 21:46:43 -0500

    carga inicial

---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.app')
+
+@section('title', 'Mi primera pagina')
+
+@section('sidebar')
+   @parent
+
+        <p>This is appended to the master sidebar.</p>
+        <h1>Hola Mundo</h1>
+@endsection
+
+@for ($i = 0; $i < 10; $i++)
+    El valor actual es {{ $i }}<br>
+@endfor
+
+@section('content')
+     <p>This is my body content.</p>
+     <h1>2018</h1>
+ @endsection
+
+
+@if( $age >= 20 )
+    <h1>Eres mayor de edad</h1>
+@elseif ($age >= 10 && $age <=15 ) 
+    <h1>Aun no cumples la edad suficiente</h1>
+@else
+    <h1>Eres menor de edad</h1>
+@endif
